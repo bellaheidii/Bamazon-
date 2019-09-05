7f95e8be3b241ff9ba790b6fe637c3726dd7c70e
Bella Fernandez <bellafernandez@Bellas-MacBook-Air.local>
2019-09-05 16:08:22 -0700

    select products

--- Bamazon-Manager/schema.sql
+++ Bamazon-Manager/schema.sql
@@ -11,3 +11,9 @@ CREATE TABLE products(
   stock_quantity INT(10) NOT NULL,
   primary key(item_id)
 );
+
+SELECT * FROM products;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Uncharted 4", "Video Games", 49.95, 150),
+  ("DOOM", "Video Games", 59.99, 200),
