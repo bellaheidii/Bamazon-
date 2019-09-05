5f680bf83e7907412286c7ffefb3eee46bbf28e8
Bella Fernandez <bellafernandez@Bellas-MacBook-Air.local>
2019-09-05 15:32:08 -0700

    bmanager.js and schema created

---
+++ Bamazon-Manager/bamazonManager.js
@@ -0,0 +1,19 @@
+// Initializes the npm packages used
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+require("console.table");
+
+// Initializes the connection variable to sync with a MySQL database
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "password",
+  database: "bamazon"
+});
