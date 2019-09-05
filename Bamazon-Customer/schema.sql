7a611651895370013ca5ce02d8702908594ab7af
Bella Fernandez <bellafernandez@Bellas-MacBook-Air.local>
2019-09-05 14:46:12 -0700

    more products

--- Bamazon-Customer/schema.sql
+++ Bamazon-Customer/schema.sql
@@ -21,4 +21,8 @@ VALUES ("Uncharted 4", "Video Games", 49.95, 150),
   ("Crate of Spam", "Food and Drink", 24.50, 50),
    ("Cool Shades", "Apparel", 75.00, 5),
   ("Worn Denim Jeans", "Apparel", 54.25, 35),
+    ("Survival Towel", "Necessities", 42.42, 42),
+  ("Bill and Ted's Excellent Adventure", "Films", 15.00, 25),
+  ("Mad Max: Fury Road", "Films", 25.50, 57),
+
 
